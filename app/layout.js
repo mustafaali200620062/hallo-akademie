02bd71fecf454179ab5d33097b992a8e8051197a
Mustafa Ali <mustafaali200620062@gmail.com>
2026-09-05 12:47:43 +0300

    move sidebar to left

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="ar"
-      dir="rtl"
+      dir="ltr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
